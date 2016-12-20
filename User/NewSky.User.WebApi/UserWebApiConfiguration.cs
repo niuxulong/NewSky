f394777d9e5f778c7a20c352f72585c02c33553f
lawrenceniu <lawrenceniu@Objectivasoftware.com>
2016-12-20 16:51:42 +0800

    add user webapi

---
+++ User/NewSky.User.WebApi/UserWebApiConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+using NewSky.Platform.Interfaces;
+
+namespace NewSky.User.WebApi
+{
+	[InheritedExport(typeof(IProductControllerConfiguration))]
+	public class UserWebApiConfiguration : IProductControllerConfiguration
+	{
+		private readonly string baseUri;
+
+		[ImportingConstructor]
+		public UserWebApiConfiguration()
+		{
+			this.baseUri = "http://127.0.0.1:8901/api/User/";
+		}
+
+		public string BaseUri
+		{
+			get { return this.baseUri; }
+		}
+	}
+}
---
+++ User/NewSky.User.WebApi/UserWebApiServiceModule.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Composition;
+using NewSky.Platform.Interfaces;
+
+namespace NewSky.User.WebApi
+{
+	[Export(typeof(IServiceModule)), PartCreationPolicy(CreationPolicy.Shared)]
+	public class UserWebApiServiceModule : IServiceModule
+    {
+		private readonly IWebApiStartupController webApiStartupController;
+		
+		[ImportingConstructor]
+		public UserWebApiServiceModule(IWebApiStartupController webApiStartupController)
+		{
+			this.webApiStartupController = webApiStartupController;
+		}
+
+		public void OnStart()
+		{
+			webApiStartupController.Start();
+		}
+
+		public void OnStop()
+		{
+			webApiStartupController.Stop();
+		}
+
+    }
+}
