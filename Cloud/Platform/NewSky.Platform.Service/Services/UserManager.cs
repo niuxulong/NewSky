b0205f8d02b4e28f6b8707715b915e533ea51dd4
lawrenceniu <lawrenceniu@Objectivasoftware.com>
2017-02-27 18:16:05 +0800

    b

---
+++ Cloud/Platform/NewSky.Platform.Service/Services/UserManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using NewSky.Platform.Api.Interfaces;
+using NewSky.Platform.Api.Models;
+using NewSky.Platform.Service.Interfaces;
+
+namespace NewSky.Platform.Service.Services
+{
+	[Export(typeof(IUserManager)), PartCreationPolicy(CreationPolicy.Shared)]
+	public class UserManager : IUserManager
+	{
+		private readonly IAuthRepository authRepository;
+
+		[ImportingConstructor]
+		public UserManager(IAuthRepository authRepository)
+		{
+			this.authRepository = authRepository;
+		}
+
+		public async Task<IdentityResult> CreateUser(User user)
+		{
+			return await authRepository.RegisterUser(user).ConfigureAwait(false);
+		}
+	}
+}
